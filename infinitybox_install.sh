21d9a4311cc4e37ff07fa3f71baddb56ccfcb411
niraj <nirajnimantha@gmail.com>
2023-12-02 22:08:01 +0530

    creating more advanced

--- infinitybox_install.sh
+++ infinitybox_install.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+# Check if running as root
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root. Please use sudo or run as root."
+    exit 1
+fi
+
+# Perform system updates
+echo "Updating system..."
+apt update && apt upgrade -y
+
 # Check if Docker is installed
 if ! command -v docker &> /dev/null; then
-    echo "Docker is not installed. Please install Docker before running this script."
-    exit 1
+    echo "Docker is not installed. Installing Docker..."
+    apt install docker.io -y
 fi
 
 # Check if Docker Compose is installed
 if ! command -v docker-compose &> /dev/null; then
-    echo "Docker Compose is not installed. Please install Docker Compose before running this script."
-    exit 1
+    echo "Docker Compose is not installed. Installing Docker Compose..."
+    apt install docker-compose -y
 fi
 
 # Check if the .env file exists
